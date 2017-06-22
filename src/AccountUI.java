2dd56a06926838656677f07cc537b067ed0c1379
Samsawesome <Samsawesome2@gmail.com>
2017-06-22 14:39:38 -0400

    Made UI
    Was able to design a UI that I like. I have a few questions about some
    things in the UI, but I understand most things.

---
+++ src/AccountUI.java
@@ -0,0 +1,110 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JTable;
+
+public class AccountUI extends JFrame {
+	private Account getBalance;
+	private Account deposit;
+	private Account withdraw;
+	private Account Account;
+	//check what to take
+	private JPanel contentPane;
+	private JTextField txtName;
+	private JTable tableAccounts;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					AccountUI frame = new AccountUI();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public AccountUI() {
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 900, 600);
+		contentPane = new JPanel();
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton btnNew = new JButton("New");
+		btnNew.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		btnNew.setBounds(12, 484, 411, 56);
+		btnNew.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+			
+			}
+		});		
+		contentPane.add(btnNew);
+		
+		JButton btnDelete = new JButton("Delete");
+		btnDelete.setFont(new Font("Tahoma", Font.PLAIN, 40));
+		btnDelete.setBounds(453, 484, 401, 56);
+		btnDelete.addActionListener(new ActionListener(){
+			public void actionPerformed(ActionEvent e){
+			
+			}
+		});
+		contentPane.add(btnDelete);
+		
+		JRadioButton rdbtnChecking = new JRadioButton("Checking Account");
+		rdbtnChecking.setEnabled(false);
+		rdbtnChecking.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		rdbtnChecking.setBounds(136, 445, 225, 25);
+		contentPane.add(rdbtnChecking);
+		
+		JRadioButton rdbtnSavings = new JRadioButton("Savings Account");
+		rdbtnSavings.setEnabled(false);
+		rdbtnSavings.setFont(new Font("Tahoma", Font.PLAIN, 25));
+		rdbtnSavings.setBounds(551, 445, 232, 25);
+		contentPane.add(rdbtnSavings);
+		
+		txtName = new JTextField();
+		txtName.setEnabled(false);
+		txtName.setFont(new Font("Tahoma", Font.PLAIN, 30));
+		txtName.setBounds(111, 346, 647, 89);
+		contentPane.add(txtName);
+		txtName.setColumns(10);
+		
+		JLabel lblQuestion = new JLabel("Hello! Welcome to the Bank! Would you like to make a new account or delete an old one?");
+		//ask how to get text to wrap so it doesnt just go in a line
+		lblQuestion.setFont(new Font("Tahoma", Font.PLAIN, 20));
+		lblQuestion.setVerticalAlignment(SwingConstants.TOP);
+		lblQuestion.setBounds(111, 277, 647, 56);
+		contentPane.add(lblQuestion);
+		
+		tableAccounts = new JTable();
+		//how to do this in general
+		tableAccounts.setEnabled(false);
+		tableAccounts.setColumnSelectionAllowed(true);
+		tableAccounts.setCellSelectionEnabled(true);
+		tableAccounts.setBounds(111, 26, 647, 170);
+		contentPane.add(tableAccounts);
+		
+	}
+}
